b8ade165e3b06c06ddd651c4164c09802f48eac7
GraceChiamaka <obieri.grace@gmail.com>
2021-02-08 23:00:42 +0100

    Add update to static pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
 import Homepage from "../pages/Homepage";
+import RegisterPage from "../pages/Auth/Register";
+import SigninPage from "../pages/Auth/Signin";
 
 const Routes = () => {
 	const history = useHistory();
 	return (
 		<BrowserRouter history={history}>
 			<Switch>
-				<Route path="/" key="home" component={Homepage} />
+				<Route path="/" exact key="home" component={Homepage} />
+				<Route path="/register" exact key="register" component={RegisterPage} />
+				<Route path="/login" exact key="signin" component={SigninPage} />
 			</Switch>
 		</BrowserRouter>
 	);
