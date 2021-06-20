91f862b67b580d5ed5b40b9cca8f6b74a1a1d8e3
GraceChiamaka <obieri.grace@gmail.com>
2021-06-20 21:55:33 +0100

    add update to pitch view

--- src/pages/Teams/index.js
+++ src/pages/Teams/index.js
@@ -6,12 +6,10 @@ import TeamEmptyState from "./EmptyState";
 import DisplayTeamList from "./DisplayTeam/ListView";
 import DisplayTeamPitch from "./DisplayTeam/PitchView";
 import { getTeam } from "../../helpers/api";
-import DashboardLayout from "../../components/common/DashboardLayout";
 
 const Teams = () => {
 	const [teamData, setTeamData] = useState(null);
 	const [loadingTeam, setLoadingTeam] = useState(false);
-	const [viewState, setViewState] = useState("view");
 
 	const view = getView();
 
@@ -24,26 +22,18 @@ const Teams = () => {
 		setLoadingTeam(true);
 		try {
 			const teams = await getTeam();
-			console.log(teams);
+
 			if (teams.statusCode === 200) {
 				setTeamData(teams.result);
-				setViewState(view);
 			}
 		} catch (error) {
 			message.error(error);
 			if (error) {
 				setLoadingTeam(false);
-				setViewState("error");
 			}
 		} finally {
 			setLoadingTeam(false);
 		}
-	};
-
-	const errorView = () => {
-		<DashboardLayout>
-			<div>Something went wrong please refresh</div>
-		</DashboardLayout>;
 	};
 
 	const handleDisplayTeams = () => {
@@ -51,22 +41,16 @@ const Teams = () => {
 		if (loadingTeam) {
 			return <div>Loading...</div>;
 		} else {
-			if (viewState === "error" && teamData === null) {
-				console.log(viewState, "==", teamData);
-
-				return errorView();
+			if (teamData === null) {
+				return <div>Loading...</div>;
 			} else {
-				if (teamData === null) {
-					return <div>Loading...</div>;
+				if (teamData.length === 0) {
+					return <TeamEmptyState />;
 				} else {
-					if (teamData.length === 0) {
-						return <TeamEmptyState />;
+					if (view === "list") {
+						return <DisplayTeamList teamInfo={teamData[0]} />;
 					} else {
-						if (view === "list") {
-							return <DisplayTeamList teamInfo={teamData[0]} />;
-						} else {
-							return <DisplayTeamPitch teamInfo={teamData[0]} />;
-						}
+						return <DisplayTeamPitch teamInfo={teamData[0]} />;
 					}
 				}
 			}
