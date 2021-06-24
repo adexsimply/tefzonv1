ce1a9f9078da957fe82de47134e693ae77ba0136
Stephen Kuforiji <stephenkuforijif@gmail.com>
2021-06-24 16:55:26 +0100

    bug fix

--- database/migrations/1623599143845_player_squad_schema.js
+++ database/migrations/1623599143845_player_squad_schema.js
@@ -14,7 +14,6 @@ class PlayerSquadSchema extends Schema {
       table.boolean("is_captain").defaultTo(0)
       table.boolean("is_substitute").default(0)
       table.string("placement")
-      table.integer("week_season_id")
       table.integer("points_total")
       table.timestamps()
     })
